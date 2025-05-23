9121e13a9d1ea9df0ab1edbbebe243706b7defe9
BounsGame <danil89106@yandex.ru>
2025-05-24 03:17:58 +0700

    Сдалал 2 ДЗ

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,53 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // 1 задание
+        var dog = 8.0 ;
+        var cat = 3.6 ;
+        var paper = 763789;
+        System.out.println("dog = " + dog + " cat = " + cat + " paper = " + paper);
+        // 2 задание
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println("dog = " + dog + " cat = " + cat + " paper = " + paper);
+        //3 задание
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println("dog = " + dog + " cat = " + cat + " paper = " + paper);
+        //4 задание
+        var friend = 19;
+        System.out.println("friend = " + friend);
+        friend = friend + 2;
+        System.out.println("friend = " + friend);
+        friend = friend / 7;
+        System.out.println("friend = " + friend);
+        //5 задание
+        var frog = 3.5;
+        System.out.println("frog = " + frog);
+        frog = frog * 10;
+        System.out.println("frog = " + frog);
+        frog = frog / 3.5;
+        System.out.println("frog = " + frog);
+        frog = frog + 4;
+        System.out.println("frog = " + frog);
+        //6 задание
+        var mass1 = 78.2;
+        var mass2 = 82.7;
+        var generalmass = mass1 + mass2;
+        System.out.println("общая масса = " + generalmass);
+        var diffmass = mass2 - mass1;
+        System.out.println("разница в массе = " + diffmass);
+        //7 задание
+        var ostatok = mass2 % mass1;
+        System.out.println("остаток от деления = " + ostatok);
+        // 8 задание
+        var hours = 640;
+        var hoursPerPerson = 8;
+        var sotrudnik = hours / hoursPerPerson;
+        System.out.println("Всего работников в компании - " + sotrudnik + " человек");
+        sotrudnik = sotrudnik + 94;
+        System.out.println("если в компании работает " + sotrudnik + " человек, то всего " + sotrudnik * hoursPerPerson + " часов работы может быть поделено между сотрудниками");
     }
 }
