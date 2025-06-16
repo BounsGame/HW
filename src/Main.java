50167c0503d52b532bc6c84976607cdeaeeac05e
BounsGame <danil89106@yandex.ru>
2025-06-16 23:54:22 +0700

    Сдалал ДЗ по масивам

--- src/Main.java
+++ src/Main.java
@@ -1,76 +1,52 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         // 1 задание
-        int bank = 0;
-        int i = 0;
-        while (bank <= 2459000) {
-            i++;
-            bank += 15000;
-            System.out.println("Месяц " + i + " сумма накоплений равна " + bank + " рублей");
-        }
+        int[] integer = new int[3];
+        integer[0] = 1;
+        integer[1] = 2;
+        integer[2] = 3;
+        double[] fractional = {1.57, 7.654, 9.986};
+        int[] five = {1, 2, 3, 4, 5};
         //2 задание
-        i = 0;
-        while (i < 10) {
-            i++;
-            System.out.print(i + " ");
+        for (int i = 0; i <= integer.length - 1; i++) {
+            if (i != integer.length - 1) System.out.print(integer[i] + ", ");
+            else System.out.print(integer[i]);
         }
         System.out.println();
-        for (; i >= 1; i--) {
-            System.out.print(i + " ");
+        for (int i = 0; i <= fractional.length - 1; i++) {
+            if (i != fractional.length - 1) System.out.print(fractional[i] + ", ");
+            else System.out.print(fractional[i]);
         }
-        //для красоты
+        System.out.println();
+        for (int i = 0; i <= five.length - 1; i++) {
+            if (i != five.length - 1) System.out.print(five[i] + ", ");
+            else System.out.print(five[i]);
+        }
         System.out.println();
         //3 задание
-        int population = 12000000;
-        int change = 17 - 8;
-        for (i = 1; i <= 10; i++) {
-            population += (population / 1000 * change);
-            System.out.println("Год " + i + " численность населения составляет " + population);
+        for (int i = 0; i <= integer.length - 1; i++) {
+            if (i != integer.length - 1) System.out.print(integer[integer.length - 1 - i] + ", ");
+            else System.out.print(integer[integer.length - 1 - i]);
         }
+        System.out.println();
+        for (int i = 0; i <= fractional.length - 1; i++) {
+            if (i != fractional.length - 1) System.out.print(fractional[fractional.length - 1 - i] + ", ");
+            else System.out.print(fractional[fractional.length - 1 - i]);
+        }
+        System.out.println();
+        for (int i = 0; i <= five.length - 1; i++) {
+            if (i != five.length - 1) System.out.print(five[five.length - 1 - i] + ", ");
+            else System.out.print(five[five.length - 1 - i]);
+        }
+        System.out.println();
         //4 задание
-        bank = 15000;
-        i = 0;
-        while (bank <= 12000000) {
-            i++;
-            bank *= 1.07;
-            System.out.println("Месяц " + i + " сумма накоплений равна " + bank + " рублей");
+        for (int i = 0; i <= integer.length - 1; i++) {
+            if (i % 2 == 0) integer[i] += 1;
         }
-        //5 задание
-        bank = 15000;
-        i = 0;
-        while (bank <= 12000000) {
-            i++;
-            bank *= 1.07;
-            if (i % 6 == 0) {
-                System.out.println("Месяц " + i + " сумма накоплений равна " + bank + " рублей");
-            }
-        }
-        //6 задание
-        bank = 15000;
-        i = 0;
-        while (i <= 108) {
-            i++;
-            bank *= 1.07;
-            if (i % 6 == 0) {
-                System.out.println("Месяц " + i + " сумма накоплений равна " + bank + " рублей");
-            }
-        }
-        //7 задание
-        int day = 1;
-        for (i = 1; i <= 31; i++) {
-            if (day == 8) day = 1;
-            if (day == 5) System.out.println("Сегодня пятница, " + i + " -е число. Необходимо подготовить отчет");
-            day += 1;
-        }
-        //8 задание
-        int actualYear = 2025;
-        int periodBegin = actualYear - 200;
-        int periodEnd = actualYear + 100;
-        while (periodBegin < periodEnd) {
-            if (periodBegin % 79 == 0) System.out.println(periodBegin);
-            periodBegin += 1;
-        }
+        System.out.println(Arrays.toString(integer));
     }
 }
