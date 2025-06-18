af0c79bbc73d2263d3aa75729e1b847a68d8131d
BounsGame <danil89106@yandex.ru>
2025-06-18 14:26:02 +0700

    Сдалал ДЗ по масивам 2 урок

--- src/Main.java
+++ src/Main.java
@@ -5,48 +5,33 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         // 1 задание
-        int[] integer = new int[3];
-        integer[0] = 1;
-        integer[1] = 2;
-        integer[2] = 3;
-        double[] fractional = {1.57, 7.654, 9.986};
-        int[] five = {1, 2, 3, 4, 5};
+        int[] budget = {15000, 10000, 12000, 25000, 18000};
+        int sum = 0;
+        for (int i : budget) {
+            sum += i;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
         //2 задание
-        for (int i = 0; i <= integer.length - 1; i++) {
-            if (i != integer.length - 1) System.out.print(integer[i] + ", ");
-            else System.out.print(integer[i]);
+        int max = budget[0];
+        int min = budget[0];
+        for (int i : budget) {
+            if (i < min) min = i;
+            if (i > max) max = i;
         }
-        System.out.println();
-        for (int i = 0; i <= fractional.length - 1; i++) {
-            if (i != fractional.length - 1) System.out.print(fractional[i] + ", ");
-            else System.out.print(fractional[i]);
-        }
-        System.out.println();
-        for (int i = 0; i <= five.length - 1; i++) {
-            if (i != five.length - 1) System.out.print(five[i] + ", ");
-            else System.out.print(five[i]);
-        }
-        System.out.println();
+        System.out.println("Минимальная сумма трат за неделю составила " + min + " рублей. Максимальная сумма трат за неделю составила " + max + " рублей");
         //3 задание
-        for (int i = 0; i <= integer.length - 1; i++) {
-            if (i != integer.length - 1) System.out.print(integer[integer.length - 1 - i] + ", ");
-            else System.out.print(integer[integer.length - 1 - i]);
-        }
-        System.out.println();
-        for (int i = 0; i <= fractional.length - 1; i++) {
-            if (i != fractional.length - 1) System.out.print(fractional[fractional.length - 1 - i] + ", ");
-            else System.out.print(fractional[fractional.length - 1 - i]);
-        }
-        System.out.println();
-        for (int i = 0; i <= five.length - 1; i++) {
-            if (i != five.length - 1) System.out.print(five[five.length - 1 - i] + ", ");
-            else System.out.print(five[five.length - 1 - i]);
-        }
-        System.out.println();
+        float average = sum / budget.length;
+        System.out.println("Средняя сумма трат за месяц составила " + average + " рублей");
         //4 задание
-        for (int i = 0; i <= integer.length - 1; i++) {
-            if (integer[i] % 2 != 0) integer[i] += 1;
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        char s;
+        for (int i = 0; i < reverseFullName.length / 2; i++) {
+            s = reverseFullName[i];
+            reverseFullName[i] = reverseFullName[reverseFullName.length - 1 - i];
+            reverseFullName[reverseFullName.length - 1 - i] = s;
         }
-        System.out.println(Arrays.toString(integer));
+        for (char i : reverseFullName) {
+            System.out.print(i);
+        }
     }
 }
