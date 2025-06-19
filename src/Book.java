fbf7f1470cc8b472d21de07c67485c27a28ff6f5
BounsGame <danil89106@yandex.ru>
2025-06-19 19:37:49 +0700

    Сдалал ДЗ по классам

---
+++ src/Book.java
@@ -0,0 +1,28 @@
+public class Book {
+    String bookName;
+    Author author;
+    int year;
+
+    public Book(String bookName, int year, Author author) {
+        this.bookName = bookName;
+        this.year = year;
+        this.author = author;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -7,53 +7,17 @@ import java.util.Locale;
 public class Main {
     public static void main(String[] args) {
         // 1 задание
-        int year = 2007;
-        checkLeanYear(year);
-        //2 задание
-        int clientOS = 1;
-        int clientDeviceYear = 2020;
-        printAppDownload(clientOS, clientDeviceYear);
-        //3 задание
-        int deliveryDistance = 27;
-        int day = calculateDeliveryDay(deliveryDistance);
-        if (day != 0) {
-            System.out.println("потребуеться дней: " + day);
-        } else {
-            System.out.println("мы не доставляем карты на дистанцию свыше 100км");
-        }
-    }
-
-    public static void checkLeanYear(int year) {
-        if ((year % 4) != 0 || year < 1584) {
-            System.out.println(year + " год — невисокосный год");
-        } else if ((year % 100) != 0 || (year % 400) == 0) {
-            System.out.println(year + " год — високосный год");
-        } else {
-            System.out.println(year + " год — невисокосный год");
-        }
-    }
-
-    public static void printAppDownload(int clientOS, int clientDeviceYear) {
-        if (clientDeviceYear < 2015 && clientOS == 0) {
-            System.out.println("установите облегчённую версию приложения для iOS по ссылке");
-        } else if (clientDeviceYear >= 2015 && clientOS == 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientDeviceYear < 2015) {
-            System.out.println("установите облегчённую версию приложения для Android по ссылке");
-        } else {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        }
-    }
-
-    public static int calculateDeliveryDay(int deliveryDistance) {
-        if (deliveryDistance < 20) {
-            return 1;
-        } else if (deliveryDistance < 60) {
-            return 2;
-        } else if (deliveryDistance < 100) {
-            return 3;
-        } else {
-            return 0;
-        }
+        String name = "Stiven";
+        String surname = "King";
+        int year = 1986;
+        String bookname = "It";
+        Author author1 = new Author(name, surname);
+        Book book1 = new Book(bookname, year, author1);
+        Author author2 = new Author("Robert", "Martin");
+        Book book2 = new Book("Clear code", 2025, author2);
+        System.out.println(book1.getBookName() + " " + book1.getYear() + " " + book1.author.getName() + " " + book1.author.getSurname());
+        System.out.println(book2.getBookName() + " " + book2.getYear() + " " + book2.author.getName() + " " + book2.author.getSurname());
+        book1.setYear(2024);
+        System.out.println(book1.getBookName() + " " + book1.getYear() + " " + book1.author.getName() + " " + book1.author.getSurname());
     }
 }
